6956df93f28a7cd3df1046ba12ca6c811d358a47
rkdalswl789 <rkdalswl789@gmail.com>
2023-10-17 15:42:24 +0900

    Feat: 포톤매니저 추가 + 박치기 기능 테스트중

---
+++ PartyIsOver/Assets/Scripts/Managers/PhotonManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0b21db6d7f7f82853b3a6553e1d671c9fdf354b818b22cd7ec90499bdcd67e9
+size 2096
--- PartyIsOver/Assets/Scripts/PlayerControl/PlayerController.cs
+++ PartyIsOver/Assets/Scripts/PlayerControl/PlayerController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:003ab9d681802c1daf975e561ca525df046132cbda01274471d15c88432ee73a
-size 2040
+oid sha256:a17e98ccc8ff68cc5b5288b2f3b5c9954ab95cdd4fdca8500a8e4cf57d0f7654
+size 2495
