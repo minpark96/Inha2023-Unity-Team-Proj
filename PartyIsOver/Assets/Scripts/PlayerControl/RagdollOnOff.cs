c28c5dd4f62f23e780182ec84241146deaa435a4
rkdalswl789 <rkdalswl789@gmail.com>
2023-10-17 09:26:09 +0900

    Feat: 플레이어 박치기 구현 진행중
    Ragdoll On OFF로 관리하기 위해 새로운 씬과 스크립트와 프리팹 추가

---
+++ PartyIsOver/Assets/Scripts/PlayerControl/RagdollOnOff.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollOnOff : MonoBehaviour
+{
+    public BoxCollider MainCollider;
+    public GameObject ThisGuysRig;
+    public Animator ThisGuysAnimator;
+    public Rigidbody ThisGuysRigidbody;
+
+
+    private void Start()
+    {
+        GetRagdollBits();
+        RagdollModeOff();
+    }
+
+    private void Update()
+    {
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            RagdollModeOn();
+            
+        }
+    }
+
+    IEnumerator GetReady()
+    {
+        yield return new WaitForSeconds(3.0f);
+    }
+
+    Collider[] RagDollColliders;
+    Rigidbody[] LimbsRigidbodies;
+
+    // Ragdoll에 원래 붙어있던 collider, rigidbody들
+    void GetRagdollBits()
+    {
+        RagDollColliders = ThisGuysRig.GetComponentsInChildren<Collider>();
+        LimbsRigidbodies = ThisGuysRig.GetComponentsInChildren<Rigidbody>();
+    }
+
+    void RagdollModeOn()
+    {
+        // Ragdoll 켜기
+        ThisGuysAnimator.enabled = false;
+
+        foreach (Collider col in RagDollColliders)
+        {
+            col.enabled = true;
+        }
+
+        foreach (Rigidbody rigid in LimbsRigidbodies)
+        {
+            rigid.isKinematic = false;
+        }
+
+        MainCollider.enabled = false;
+        ThisGuysRigidbody.isKinematic = true;
+    }
+
+    
+    void RagdollModeOff()
+    {
+        // Ragdoll 끄기
+
+        foreach(Collider col in RagDollColliders)
+        {
+            col.enabled = false;
+        }
+
+        foreach (Rigidbody rigid in LimbsRigidbodies)
+        {
+            rigid.isKinematic = true;
+        }
+
+        ThisGuysAnimator.enabled = true;
+        MainCollider.enabled = true;
+        ThisGuysRigidbody.isKinematic = false;
+    }
+
+
+
+}
