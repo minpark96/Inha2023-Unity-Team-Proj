d665e49db91958c25cb01902a387d7e753ce302b
minpark96 <hmpark3355@gmail.com>
2023-10-18 20:50:21 +0900

    Feat: 플레이어 커넥션 관찰 기능 추가 Photon PUN Basics Tutorial 4 - Game Manager & Levels 중  Watching Players Connection 구현이다.

--- PartyIsOver/Assets/Scripts/PhotonTutorial/GameManager.cs
+++ PartyIsOver/Assets/Scripts/PhotonTutorial/GameManager.cs
@@ -13,12 +13,36 @@ namespace Photon.Tutorial
     {
         #region Photon Callbacks
 
-        /// <summary>
-        /// Called when the local player left the room. We need to load the launcher scene.
-        /// </summary>
-        public override void OnLeftRoom()
+        ///// <summary>
+        ///// Called when the local player left the room. We need to load the launcher scene.
+        ///// </summary>
+        //public override void OnLeftRoom()
+        //{
+        //    SceneManager.LoadScene(0);
+        //}
+
+        public override void OnPlayerEnteredRoom(Player other)
         {
-            SceneManager.LoadScene(0);
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
         }
 
         #endregion
