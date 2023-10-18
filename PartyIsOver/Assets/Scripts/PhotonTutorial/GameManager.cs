3682ae9dbc63e5fa1c3eca732409a59aa61056a1
minpark96 <hmpark3355@gmail.com>
2023-10-18 17:23:13 +0900

    Feat: GameManager, Leave 버튼, Room 씬 1~4, 빌드 세팅 추가 Photon PUN Basics Tutorial 3 - Game Scenes 중 1. Game Manager 2. Room Scenes 3. Quit Button 4. Build Settings 구현이다.

---
+++ PartyIsOver/Assets/Scripts/PhotonTutorial/GameManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Photon.Tutorial
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        #region Photon Callbacks
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+    }
+}
