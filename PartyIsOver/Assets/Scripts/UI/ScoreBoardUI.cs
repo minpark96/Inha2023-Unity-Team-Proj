870f323325cd8a90041763137e7c4597690f0694
rkdalswl789 <rkdalswl789@gmail.com>
2023-12-02 16:32:52 +0900

    Feat: Tab 이미지 ON/OFF 추가

--- PartyIsOver/Assets/Scripts/UI/ScoreBoardUI.cs
+++ PartyIsOver/Assets/Scripts/UI/ScoreBoardUI.cs
@@ -12,6 +12,7 @@ public class ScoreBoardUI : MonoBehaviour
 
     public GameObject Info;
     public Sprite PointStar;
+    public GameObject Tab;
 
     private GameObject _scoreBoardPanel;
     private GameObject[] _portrait = new GameObject[6];
@@ -82,6 +83,7 @@ public class ScoreBoardUI : MonoBehaviour
                     if (Input.GetKeyDown(KeyCode.Tab))
                     {
                         OnClickScoreBoardON();
+                        Tab.SetActive(false);
                     }
                 }
                 break;
@@ -91,6 +93,7 @@ public class ScoreBoardUI : MonoBehaviour
                     if (Input.GetKeyUp(KeyCode.Tab))
                     {
                         OnClickScoreBoardOFF();
+                        Tab.SetActive(true);
                     }
                 }
                 break;
