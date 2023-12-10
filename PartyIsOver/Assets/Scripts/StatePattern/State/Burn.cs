dcff56b6c03ecde04623168c4bab75352fbf7cd3
sdj3701 <110330701+sdj3701@users.noreply.github.com>
2023-12-10 12:07:50 +0900

    Feat : 화상 작업중

--- PartyIsOver/Assets/Scripts/StatePattern/State/Burn.cs
+++ PartyIsOver/Assets/Scripts/StatePattern/State/Burn.cs
@@ -9,11 +9,22 @@ public class Burn : MonoBehaviourPun , IDebuffState
     public float CoolTime { get; set; }
     public GameObject effectObject { get; set; }
     public Transform playerTransform { get; set; }
-    
+
+    AudioClip _audioClip = null;
+    AudioSource _audioSource;
+    float delay = 3f;
+    float _burnDamage = 5f;
+
     public void EnterState()
     {
         effectObject = null;
         playerTransform = this.transform.Find("GreenHip").GetComponent<Transform>();
+        InstantiateEffect("Effects/Fire_large");
+        PlayerDebuffSound("PlayerEffect/SFX_FireBall_Projectile");
+        TransferDebuffToPlayer((int)InteractableObject.Damage.Burn);
+
+
+
     }
 
     public void UpdateState()
@@ -26,12 +37,107 @@ public class Burn : MonoBehaviourPun , IDebuffState
     {
 
     }
+
+    IEnumerator BurnDmanage()
+    {
+        float elapsedTime = 0f;
+        float lastBurnTime = Time.time;
+        float startTime = Time.time;
+
+        while (elapsedTime <= delay)
+        {
+            if (MyActor.debuffState == Actor.DebuffState.Ice)
+            {
+                //_hasBurn = false;
+                //상태가 Ice 가 들어오면 종료를 해야한다.
+                MyActor.actorState = Actor.ActorState.Stand;
+                //StopCoroutine(Burn(delay));
+            }
+
+            if (Time.time - lastBurnTime >= 1.0f) // 1초간 데미지+액션
+            {
+                MyActor.Health -= _burnDamage;
+                MyActor.BodyHandler.Waist.PartRigidbody.AddForce((MyActor.BodyHandler.Hip.transform.right) * 40f, ForceMode.VelocityChange);
+                MyActor.BodyHandler.Hip.PartRigidbody.AddForce((MyActor.BodyHandler.Hip.transform.right) * 40f, ForceMode.VelocityChange);
+                lastBurnTime = Time.time;
+            }
+
+            elapsedTime = Time.time - startTime;
+            yield return null;
+        }
+    }
+
     public void InstantiateEffect(string path)
     {
-
+        effectObject = Managers.Resource.PhotonNetworkInstantiate($"{path}");
     }
     public void RemoveObject(string name)
     {
-
+        GameObject go = GameObject.Find($"{name}");
+        Managers.Resource.Destroy(go);
+        effectObject = null;
+    }
+    void PlayerDebuffSound(string path)
+    {
+        _audioClip = Managers.Sound.GetOrAddAudioClip(path);
+        _audioSource.clip = _audioClip;
+        _audioSource.volume = 0.2f;
+        _audioSource.spatialBlend = 1;
+        Managers.Sound.Play(_audioClip, Define.Sound.PlayerEffect, _audioSource);
+    }
+    public void TransferDebuffToPlayer(int DamageType)
+    {
+        ChangeDamageModifier((int)Define.BodyPart.LeftFoot, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.RightFoot, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.LeftLeg, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.RightLeg, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.Head, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.LeftHand, DamageType);
+        ChangeDamageModifier((int)Define.BodyPart.RightHand, DamageType);
+    }
+    private void ChangeDamageModifier(int bodyPart, int DamageType)
+    {
+        switch ((Define.BodyPart)bodyPart)
+        {
+            case Define.BodyPart.LeftFoot:
+                MyActor.BodyHandler.LeftFoot.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.RightFoot:
+                MyActor.BodyHandler.RightFoot.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.LeftLeg:
+                MyActor.BodyHandler.LeftLeg.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.RightLeg:
+                MyActor.BodyHandler.RightLeg.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.LeftThigh:
+                break;
+            case Define.BodyPart.RightThigh:
+                break;
+            case Define.BodyPart.Hip:
+                break;
+            case Define.BodyPart.Waist:
+                break;
+            case Define.BodyPart.Chest:
+                break;
+            case Define.BodyPart.Head:
+                MyActor.BodyHandler.Head.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.LeftArm:
+                break;
+            case Define.BodyPart.RightArm:
+                break;
+            case Define.BodyPart.LeftForeArm:
+                break;
+            case Define.BodyPart.RightForeArm:
+                break;
+            case Define.BodyPart.LeftHand:
+                MyActor.BodyHandler.LeftHand.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+            case Define.BodyPart.RightHand:
+                MyActor.BodyHandler.RightHand.PartInteractable.damageModifier = (InteractableObject.Damage)DamageType;
+                break;
+        }
     }
 }
