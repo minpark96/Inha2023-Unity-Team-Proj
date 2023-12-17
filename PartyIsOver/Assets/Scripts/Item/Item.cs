d18e017fb4e8ad76c1681878bb1f240bf8b1a233
k05n17j <k05n17j@gmail.com>
2023-12-18 03:12:08 +0900

    Feat:아이템던지기 임시

--- PartyIsOver/Assets/Scripts/Item/Item.cs
+++ PartyIsOver/Assets/Scripts/Item/Item.cs
@@ -58,40 +58,47 @@ public class Item : MonoBehaviourPun
         yield return new WaitForSeconds(0.3f);
         gameObject.SetActive(false);
         yield return new WaitForSeconds(0.3f);
-        
 
-        ProjectileBase projectile = Managers.Pool.Pop(ItemData.Projectile.gameObject).GetComponent<ProjectileBase>();
-        projectile.gameObject.layer = Owner.gameObject.layer;
+        if(photonView.IsMine)
+        {
+            //ProjectileBase projectile = Managers.Pool.Pop(ItemData.Projectile.gameObject).GetComponent<ProjectileBase>();
+            object[] instantiationData = new object[] { photonView.ViewID };
+            ProjectileBase projectile = PhotonNetwork.Instantiate($"Prefabs/Projectiles/MushroomProjectile", Vector3.zero, Quaternion.identity, 0,instantiationData).GetComponent<ProjectileBase>();
 
-        Vector3 forward = new Vector3(-Owner.BodyHandler.Head.PartTransform.up.x,0f, -Owner.BodyHandler.Head.PartTransform.up.z).normalized;
-        Vector3 right = new Vector3(Owner.BodyHandler.Head.PartTransform.right.z, 0f, Owner.BodyHandler.Head.PartTransform.right.z).normalized;
-        projectile.transform.position = Owner.BodyHandler.Chest.transform.position + (forward * 0.2f) + (Vector3.up * 0.1f) +(right *0.2f);
-
-        Vector3 pos1 = Owner.BodyHandler.Chest.transform.position + forward * 10f + Vector3.up * 2f + right * 3f;
+            projectile.gameObject.layer = Owner.gameObject.layer;
 
 
-        Transform camArm = Owner.CameraControl.CameraArm;
-        Vector3 lookForward = new Vector3(camArm.forward.x, 0f, camArm.forward.z).normalized;
-        Vector3 lookRight = new Vector3(camArm.right.x, 0f, camArm.right.z).normalized;
+            Vector3 forward = new Vector3(-Owner.BodyHandler.Head.PartTransform.up.x, 0f, -Owner.BodyHandler.Head.PartTransform.up.z).normalized;
+            Vector3 right = new Vector3(Owner.BodyHandler.Head.PartTransform.right.z, 0f, Owner.BodyHandler.Head.PartTransform.right.z).normalized;
+            projectile.transform.position = Owner.BodyHandler.Chest.transform.position + (forward * 0.2f) + (Vector3.up * 0.1f) + (right * 0.2f);
 
-        Vector3 input = Owner.PlayerController.MoveInput;
-        Vector3 moveDir = lookForward * input.z + lookRight * input.x;
-
-
-   
-        Vector3 pos2 = Owner.BodyHandler.Chest.transform.position + moveDir.normalized * 10f + Vector3.up * 3f;
-
-        if (input.magnitude == 0f)
-            projectile.transform.LookAt(pos1);
-        else
-            projectile.transform.LookAt(pos2);
+            Vector3 pos1 = Owner.BodyHandler.Chest.transform.position + forward * 10f + Vector3.up * 2f + right * 3f;
 
 
-        projectile.Shoot(this);
-        
+            Transform camArm = Owner.CameraControl.CameraArm;
+            Vector3 lookForward = new Vector3(camArm.forward.x, 0f, camArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(camArm.right.x, 0f, camArm.right.z).normalized;
+
+            Vector3 input = Owner.PlayerController.MoveInput;
+            Vector3 moveDir = lookForward * input.z + lookRight * input.x;
+
+
+
+            Vector3 pos2 = Owner.BodyHandler.Chest.transform.position + moveDir.normalized * 10f + Vector3.up * 3f;
+
+            if (input.magnitude == 0f)
+                projectile.transform.LookAt(pos2);
+            else
+                projectile.transform.LookAt(pos2);
+            projectile.Shoot(this);
+
+            projectile.photonView.RPC("ChangeUseTypeTrigger", RpcTarget.MasterClient, 0f, -1f);
+            //projectile.GetComponent<InteractableObject>().ChangeUseTypeTrigger(0.08f, -1f);
+        }
+
+
         Owner.PlayerController.PlayerEffectSound("PlayerEffect/Cartoon-UI-040");
 
-        projectile.GetComponent<InteractableObject>().ChangeUseTypeTrigger(0.08f, -1f);
     }
 }
 
